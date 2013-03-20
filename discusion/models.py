953e854838d18e41931ac4a318975beba6618ae9
harveydf <harvey.forero@gmail.com>
2013-03-19 21:14:08 -0500

    Revert "Creación modelo de discusiones y el inclusion tag"
    This reverts commit 7bd601e327d9eb26d9de6af0f1897749f393b6ff.

--- discusion/models.py
+++ discusion/models.py
@@ -1,14 +1,3 @@
 from django.db import models
 
-class Preguntas(models.Model):
-	titulo = models.CharField(max_length=255)
-
-	def __unicode__(self):
-		return self.titulo
-
-class Respuestas(models.Model):
-	titulo = models.CharField(max_length=255)
-	pregunta = models.ForeignKey(Preguntas)
-
-	def __unicode__(self):
-		return self.titulo
+# Create your models here.
--- discusion/templatetags/discusiones_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.inclusion_tag('discusiones.html')
-def show_discusiones():
-	return
-
