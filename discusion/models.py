7bd601e327d9eb26d9de6af0f1897749f393b6ff
harveydf <harvey.forero@gmail.com>
2013-03-19 21:08:53 -0500

    Creación modelo de discusiones y el inclusion tag

--- discusion/models.py
+++ discusion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Preguntas(models.Model):
+	titulo = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return self.titulo
+
+class Respuestas(models.Model):
+	titulo = models.CharField(max_length=255)
+	pregunta = models.ForeignKey(Preguntas)
+
+	def __unicode__(self):
+		return self.titulo
